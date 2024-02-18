f683a947dce4afdc68c103235c200c287b242649
Mohammad Roshangara <mroshangara@gmail.com>
2024-02-18 18:06:28 +0330

    refactor: make some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "../Webfonts/style.css";
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,46 @@
+import Modal from "./category/(components)/Modal";
+
+export async function getData() {
+  const res = await fetch("https://random-word-api.herokuapp.com/word");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
 
 
-export default function Home() {
-  return <div> yo </div>;
+export default async function Page() {
+  const categories = [
+    "احساسات",
+    "شخصیت",
+    "مکان",
+    "شغل",
+    "حیوان",
+    "غذا",
+    "فعالیت",
+    "اشیا",
+  ];
+  const data = await getData();
+
+  return (
+    <main className="p-5 flex justify-center flex-col text-center gap-16 bg-black h-screen bg-gradient-to-b from-purple-300 to-purple-50">
+      <div className="flex flex-col gap-5">
+        <h1 className="text-purple-800"> دسته‌بندی خودت رو انتخاب کن </h1>
+        <p id="data">{data}</p>
+        <button className="bg-white border-2 border-purple-800 text-purple-800 p-4 py-2.5 rounded-xl font-semibold">
+          انتخاب تصادفی
+        </button>
+      </div>
+      <section className="flex flex-wrap gap-4 justify-center">
+        {categories.map((category, index) => (
+          <Modal key={index} category={category} />
+        ))}
+      </section>
+    </main>
+  );
 }
