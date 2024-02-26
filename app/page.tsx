78443b5c43aef328e81e4ab5b82400e95220c38e
Kiana Nb <kiana.nb34@gmail.com>
2024-02-26 13:00:04 +0330

    v1 done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const categories = [
     {
-      id: "TE",
+      id: "TF",
       title: "احساسات",
       icon: "/assets/emotions.png",
     },
@@ -19,12 +22,12 @@ export default function Page() {
       icon: "/assets/books.png",
     },
     {
-      id: "TEN",
+      id: "TE",
       title: "محیط",
       icon: "/assets/environment.png",
     },
     {
-      id: "TF",
+      id: "TCH",
       title: "شخصیت",
       icon: "/assets/figures.png",
     },
@@ -39,8 +42,8 @@ export default function Page() {
       icon: "/assets/objects.png",
     },
     {
-      id: "TP",
-      title: "مکان",
+      id: "TT",
+      title: "گردشگری",
       icon: "/assets/places.png",
     },
     {
@@ -49,12 +52,12 @@ export default function Page() {
       icon: "/assets/sports.png",
     },
     {
-      id: "TAB",
+      id: "TET",
       title: "انتزاعی",
       icon: "/assets/abstract.png",
     },
     {
-      id: "TT",
+      id: "TTE",
       title: "تکنولوژی",
       icon: "/assets/technology.png",
     },
@@ -74,16 +77,42 @@ export default function Page() {
       icon: "/assets/city-country.png",
     },
     {
-      id: "TPR",
+      id: "TZ",
       title: " ضرب‌المثل ",
       icon: "/assets/proverb.png",
     },
     {
-      id: "TCR",
+      id: "TK",
       title: " کودکان ",
       icon: "/assets/children.png",
     },
   ];
+
+  const [randomCategoryId, setRandomCategoryId] = useState("");
+
+  // Function to choose a random category ID
+  const chooseRandomCategory = () => {
+    const randomIndex = Math.floor(Math.random() * categories.length);
+    const randomCategory = categories[randomIndex];
+    setRandomCategoryId(randomCategory.id);
+  };
+
+  // Function to generate a random number between min (inclusive) and max (inclusive)
+  const generateRandomNumber = (min: any, max: any) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  // Effect to choose a random category ID on component mount
+  useEffect(() => {
+    chooseRandomCategory();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Function to generate the link with the random category ID and random level
+  const generateRandomLink = () => {
+    const randomLevel = generateRandomNumber(1, 3);
+    return `/category/${randomCategoryId}/level/${randomLevel}`;
+  };
 
   return (
     <main className="p-4 flex justify-center flex-col items-center gap-16 h-full py-10">
@@ -98,7 +127,11 @@ export default function Page() {
       </h1>
       <section className="flex flex-col gap-4 bg-white/70 rounded-3xl p-4">
         <section className="flex gap-3">
-          <button className="bg-gray-50 px-4 shadow-lg py-2.5 rounded-2xl w-full flex justify-center gap-4 items-center hover:scale-105 transition-transform active:translate-y-1">
+          <Link
+            href={generateRandomLink()}
+            passHref
+            className="bg-gray-50 px-4 shadow-lg py-2.5 rounded-2xl w-full flex justify-center gap-4 items-center hover:scale-105 transition-transform active:translate-y-1"
+          >
             <Image
               alt="icon image"
               src="/assets/dice.png"
@@ -107,8 +140,11 @@ export default function Page() {
               className="w-7 h-7"
             />
             <p className="text-sm font-medium lg:text-base"> تصادفی </p>
-          </button>
-          <button className="bg-gray-50 px-4 py-2.5 rounded-2xl  w-full flex justify-center gap-4 items-center shadow-[0_10px_15px_-3px_rgba(243,183,29,0.2),0_4px_6px_-4px_rgba(243,183,29,0.2)] border border-yellow-400 hover:scale-105 transition-transform active:translate-y-1">
+          </Link>
+          <Link
+            href={"/category/tg/level/4"}
+            className="bg-gray-50 px-4 py-2.5 rounded-2xl  w-full flex justify-center gap-4 items-center shadow-[0_10px_15px_-3px_rgba(243,183,29,0.2),0_4px_6px_-4px_rgba(243,183,29,0.2)] border border-yellow-400 hover:scale-105 transition-transform active:translate-y-1"
+          >
             <Image
               alt="icon image"
               src="/assets/golden-cup.png"
@@ -117,19 +153,26 @@ export default function Page() {
               className="w-7 h-7"
             />
             <p className="text-sm font-medium lg:text-base"> طلایی </p>
-          </button>
+          </Link>
         </section>
         <section className="grid grid-cols-4 gap-2">
           {categories.map((category, index) => (
             <Link
-            key={index}
-            href={`/category/${category.id}`}
-            passHref
-            className="bg-gray-50 flex flex-col justify-center items-center h-full w-full p-4 gap-3 shadow-lg rounded-2xl  hover:shadow-2xl  hover:scale-105 transition-transform active:translate-y-1"
-          >
-            <Image alt="icon image" src={category.icon} width={100} height={100} />
-            <p className="text-xs font-medium lg:text-base">{category.title}</p>
-          </Link>
+              key={index}
+              href={`/category/${category.id}`}
+              passHref
+              className="bg-gray-50 flex flex-col justify-center items-center h-full w-full p-4 gap-3 shadow-lg rounded-2xl  hover:shadow-2xl  hover:scale-105 transition-transform active:translate-y-1"
+            >
+              <Image
+                alt="icon image"
+                src={category.icon}
+                width={100}
+                height={100}
+              />
+              <p className="text-xs font-medium lg:text-base">
+                {category.title}
+              </p>
+            </Link>
           ))}
         </section>
       </section>
