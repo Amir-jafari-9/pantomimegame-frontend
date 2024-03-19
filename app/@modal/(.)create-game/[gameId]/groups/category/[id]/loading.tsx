290bc9e4c2a16c37664cd136798bb257e8b65444
Kiana Nb <kiana.nb34@gmail.com>
2024-03-19 17:01:35 +0330

    fix:bugs

--- app/@modal/(.)create-game/[gameId]/groups/category/[id]/loading.tsx
+++ app/@modal/(.)create-game/[gameId]/groups/category/[id]/loading.tsx
@@ -7,7 +7,7 @@ export default function Loading() {
   // Or a custom loading skeleton component
   return (
     <div className="fixed inset-0 z-50 bg-black/50">
-      <div className="flex flex-col items-center justify-center text-center fixed left-[50%] top-[50%] z-50 max-w-lg translate-x-[-50%] translate-y-[-50%]  sm:rounded-lg w-full h-full">
+      <div className="flex flex-col items-center justify-center text-center fixed left-[50%] top-[50%] z-50 max-w-lg translate-x-[-50%] translate-y-[-50%]  sm:rounded-lg w-full lg:w-full h-full">
         <Lottie animationData={animationData} className="w-full px-10" />
       </div>
     </div>
